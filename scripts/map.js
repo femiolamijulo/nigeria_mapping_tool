c3db7ad738a555db06b85639d3d8ce5846ca8d24
Femi Olamijulo <olufemi_olamijulo@gsd.harvard.edu>
2025-03-07 17:20:56 -0500

    modified:   index.html 	modified:   scripts/map.js

---
+++ scripts/map.js
@@ -0,0 +1,8 @@
+/**
+ * Map-related functionality for Nigeria Map Tool
+ */
+
+// This file can be used to add more map-specific functionality in the future
+// Currently the search box functionality is handled in sidebar.js
+
+export {};
