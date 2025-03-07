b385d3611ca97f48080307c13cdd905795de655c
Femi Olamijulo <olufemi_olamijulo@gsd.harvard.edu>
2025-03-07 17:44:21 -0500

    modified:   index.html 	modified:   scripts/map.js

--- scripts/map.js
+++ scripts/map.js
@@ -10,6 +10,9 @@ fetch('ward_geojson.geojson')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data).addTo(map);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON data:', error);
     });
 
 // This file can be used to add more map-specific functionality in the future
